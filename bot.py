3a26a12f7fca30045c9e4b9592906f7286ed879c
plutone <borelliplutone@gmail.com>
2020-08-16 19:04:51 +0200

    shipping command, need to test with real users and create timestamp

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import json
+import random
 from telegram.ext import Updater, CommandHandler, CallbackContext
 from telegram import Update
 from dotenv import load_dotenv
@@ -19,14 +20,14 @@ def setup_shippering_file(update: Update, context: CallbackContext):
         chat_administrators = context.bot.get_chat_administrators(chat_id=update.effective_chat.id)
         with open('counters.json', 'r+') as counters_fp:
             try:
-                json.load(counters_fp)
+                counters = json.load(counters_fp)
             except json.JSONDecodeError as e:
                 logging.warning(e)
                 # empty file
                 chat_ship = {}
                 chat_ship[update.effective_chat.id] = {}
                 for chat_member in chat_administrators:
-                    chat_ship[update.effective_chat.id][chat_member.user.user_id] = 0
+                    chat_ship[update.effective_chat.id][chat_member.user.id] = 0
                 json.dump(chat_ship, counters_fp)
 
 
@@ -48,6 +49,33 @@ def help(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
 
+def shipping(update: Update, context: CallbackContext):
+    setup_shippering_file(update, context)
+    with open('counters.json') as counters_fp:
+        try:
+            counters = json.load(counters_fp)
+
+            ship1, ship2 = tuple(random.sample(range(0, len(counters[str(update.effective_chat.id)].keys())), k=2))
+
+            user_id_shipped1 = list(counters[str(update.effective_chat.id)].items())[ship1][0]
+            user_id_shipped2 = list(counters[str(update.effective_chat.id)].items())[ship2][0]
+            counters[str(update.effective_chat.id)][user_id_shipped1] += 1
+            counters[str(update.effective_chat.id)][user_id_shipped2] += 1
+
+            logging.info(str(counters))
+            with open('counters.json', 'w') as _counters_fp:
+                json.dump(counters, _counters_fp)
+
+            text = 'La coppia del giorno:' \
+                f'{context.bot.get_chat_member(chat_id=update.effective_chat.id, user_id=user_id_shipped1)} + {context.bot.get_chat_member(chat_id=update.effective_chat.id, user_id=user_id_shipped2)} = ❤️' \
+                f'La nuova coppia del giorno potrà essere scelta tra 24 ore'
+
+            context.bot.send_message(chat_id=update.effective_chat.id, text=text)
+        except json.JSONDecodeError as e:
+            logging.error("UNABLE TO READ FILE")
+            context.bot.send_message(chat_id=update.effective_chat.id, text='Oops, something went wrong 😅')
+
+
 def main():
 
     updater = Updater(token=token, use_context=True)
@@ -55,9 +83,11 @@ def main():
     dispatcher = updater.dispatcher
     start_handler = CommandHandler('start', start)
     help_handler = CommandHandler('help', help)
+    ship_handler = CommandHandler('shipping', shipping)
 
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(help_handler)
+    dispatcher.add_handler(ship_handler)
 
     updater.start_polling()
 
