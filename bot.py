b9a20c81d762d987df3c165a17678285a2c1008d
plutone <borelliplutone@gmail.com>
2020-08-18 20:28:04 +0200

    tag name with shipping and fixed timed action

--- bot.py
+++ bot.py
@@ -15,12 +15,14 @@ logging.basicConfig(level=logging.DEBUG,
 
 token = os.getenv('TELEGRAM_TOKEN')
 
-shippable = True
+chat_id = ''
 deadline = datetime(datetime.today().year, datetime.today().month, datetime.today().day, hour=17)
 
 
 def setup_shippering_file(update: Update, context: CallbackContext):
     if update.effective_chat.type != 'private':
+        global chat_id
+        chat_id = str(update.effective_chat.id)
         # initialize to 0 shipping counter if the file is empty
         chat_administrators = context.bot.get_chat_administrators(chat_id=update.effective_chat.id)
         with open('counters.json', 'r+') as counters_fp:
@@ -31,6 +33,7 @@ def setup_shippering_file(update: Update, context: CallbackContext):
                 # empty file
                 chat_ship = {}
                 chat_ship[update.effective_chat.id] = {}
+                chat_ship[update.effective_chat.id]['shippable'] = True
                 chat_ship[update.effective_chat.id]['user_counters'] = {}
                 for chat_member in chat_administrators:
                     chat_ship[update.effective_chat.id]['user_counters'][chat_member.user.id] = 0
@@ -61,60 +64,66 @@ def help(update: Update, context: CallbackContext):
 
 
 def shipping(update: Update, context: CallbackContext):
-    global shippable
     setup_shippering_file(update, context)
     with open('counters.json') as counters_fp:
         try:
             counters = json.load(counters_fp)
 
-            if shippable:
-                # last key is not counted for randomization since it's the last ship
-                ship1, ship2 = tuple(
-                    random.sample(range(0, len(counters[str(update.effective_chat.id)]['user_counters'].keys())), k=2))
-
-                # find the id whose index in the key list is the rng number
-                user_id_shipped1 = list(counters[str(update.effective_chat.id)]['user_counters'].items())[ship1][0]
-                user_id_shipped2 = list(counters[str(update.effective_chat.id)]['user_counters'].items())[ship2][0]
-
-                counters[str(update.effective_chat.id)]['user_counters'][user_id_shipped1] += 1
-                counters[str(update.effective_chat.id)]['user_counters'][user_id_shipped2] += 1
-                counters[str(update.effective_chat.id)]['last_couple'].append(user_id_shipped1)
-                counters[str(update.effective_chat.id)]['last_couple'].append(user_id_shipped2)
-                if len(counters[str(update.effective_chat.id)]['last_couple']) > 10:
-                    counters[str(update.effective_chat.id)]['last_couple'].pop(0)
-                    counters[str(update.effective_chat.id)]['last_couple'].pop(0)
-
-                # write updates
-                with open('counters.json', 'w') as _counters_fp:
-                    json.dump(counters, _counters_fp)
-            else:
-                # if the ship for today is picked, only need to pop from the stack
-                user_id_shipped1 = counters[str(update.effective_chat.id)]['last_couple'].pop()
-                user_id_shipped2 = counters[str(update.effective_chat.id)]['last_couple'].pop()
-
-            shippable = False
-
-            # find out how much needs to be waited to ship again
-            now = datetime.today()
-            time_to_wait = deadline - now
-            total_seconds = time_to_wait.seconds + math.floor(round(time_to_wait.microseconds / 1000000, 1))
-            hours, remaining_seconds = divmod(total_seconds, 3600)
-            minutes, seconds = divmod(remaining_seconds, 60)
-
-            first_name1 = context.bot.get_chat_member(chat_id=update.effective_chat.id, user_id=user_id_shipped1).user.first_name
-            last_name1 = context.bot.get_chat_member(chat_id=update.effective_chat.id, user_id=user_id_shipped1).user.last_name
-            first_name2 = context.bot.get_chat_member(chat_id=update.effective_chat.id, user_id=user_id_shipped2).user.first_name
-            last_name2 = context.bot.get_chat_member(chat_id=update.effective_chat.id, user_id=user_id_shipped2).user.last_name
-            logging.info(shippable)
-            text = 'La coppia del giorno:\n\n' if shippable else 'La coppia del giorno è già stata scelta:\n\n'
-            text += f'{first_name1} {last_name1} ' if last_name1 else f'{first_name1} '
-            text += f'+ {first_name2} {last_name2} ' if last_name2 else f'+ {first_name2} = ❤\n'
-            text += f'La nuova coppia del giorno potrà essere scelta tra {hours} ore, {minutes} minuti e {seconds} secondi'
-
-            context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode='HTML')
         except json.JSONDecodeError as e:
             logging.error("UNABLE TO READ FILE")
             context.bot.send_message(chat_id=update.effective_chat.id, text='Oops, something went wrong 😅', parse_mode='HTML')
+        logging.info(counters[str(update.effective_chat.id)]['last_couple'])
+        if counters[str(update.effective_chat.id)]['shippable']:
+            # last key is not counted for randomization since it's the last ship
+            ship1, ship2 = tuple(
+                random.sample(range(0, len(counters[str(update.effective_chat.id)]['user_counters'].keys())), k=2))
+
+            # find the id whose index in the key list is the rng number
+            user_id_shipped1 = list(counters[str(update.effective_chat.id)]['user_counters'].items())[ship1][0]
+            user_id_shipped2 = list(counters[str(update.effective_chat.id)]['user_counters'].items())[ship2][0]
+
+            counters[str(update.effective_chat.id)]['user_counters'][user_id_shipped1] += 1
+            counters[str(update.effective_chat.id)]['user_counters'][user_id_shipped2] += 1
+            counters[str(update.effective_chat.id)]['last_couple'].append(user_id_shipped1)
+            counters[str(update.effective_chat.id)]['last_couple'].append(user_id_shipped2)
+            if len(counters[str(update.effective_chat.id)]['last_couple']) > 10:
+                counters[str(update.effective_chat.id)]['last_couple'].pop(0)
+                counters[str(update.effective_chat.id)]['last_couple'].pop(0)
+
+            counters[str(update.effective_chat.id)]['shippable'] = False
+            # write updates
+            with open('counters.json', 'w') as _counters_fp:
+                json.dump(counters, _counters_fp)
+
+
+        else:
+            # if the ship for today is picked, only need to pop from the stack
+            user_id_shipped2 = counters[str(update.effective_chat.id)]['last_couple'].pop()
+            user_id_shipped1 = counters[str(update.effective_chat.id)]['last_couple'].pop()
+        logging.info(counters[str(update.effective_chat.id)]['last_couple'])
+
+
+        # find out how much needs to be waited to ship again
+        now = datetime.today()
+        time_to_wait = deadline - now
+        total_seconds = time_to_wait.seconds + math.floor(round(time_to_wait.microseconds / 1000000, 1))
+        hours, remaining_seconds = divmod(total_seconds, 3600)
+        minutes, seconds = divmod(remaining_seconds, 60)
+
+        first_name1 = context.bot.get_chat_member(chat_id=update.effective_chat.id,
+                                                  user_id=user_id_shipped1).user.first_name
+        last_name1 = context.bot.get_chat_member(chat_id=update.effective_chat.id,
+                                                 user_id=user_id_shipped1).user.last_name
+        first_name2 = context.bot.get_chat_member(chat_id=update.effective_chat.id,
+                                                  user_id=user_id_shipped2).user.first_name
+        last_name2 = context.bot.get_chat_member(chat_id=update.effective_chat.id,
+                                                 user_id=user_id_shipped2).user.last_name
+        text = 'La coppia del giorno:\n\n' if counters[str(update.effective_chat.id)]['shippable'] else 'La coppia del giorno è già stata scelta:\n\n'
+        text += f'<a href="tg://user?id={user_id_shipped1}">{first_name1} {last_name1}</a> ' if last_name1 else f'<a href="tg://user?id={user_id_shipped1}">{first_name1}</a>'
+        text += f'+ <a href="tg://user?id={user_id_shipped2}">{first_name2} {last_name2}</a> = ❤\n' if last_name2 else f'+ <a href="tg://user?id={user_id_shipped2}">{first_name2}</a> = ❤\n'
+        text += f'La nuova coppia del giorno potrà essere scelta tra {hours} ore, {minutes} minuti e {seconds} secondi'
+
+        context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode='HTML')
 
 
 def last_ship(update: Update, context: CallbackContext):
@@ -124,8 +133,8 @@ def last_ship(update: Update, context: CallbackContext):
         counters = json.load(counters_fp)
         last_couple_stack = counters[str(update.effective_chat.id)]['last_couple']
         for i in range(0, len(counters[str(update.effective_chat.id)]['last_couple']), 2):
-            user_id_shipped1 = last_couple_stack.pop()
             user_id_shipped2 = last_couple_stack.pop()
+            user_id_shipped1 = last_couple_stack.pop()
             first_name1 = context.bot.get_chat_member(chat_id=update.effective_chat.id,
                                                       user_id=user_id_shipped1).user.first_name
             last_name1 = context.bot.get_chat_member(chat_id=update.effective_chat.id,
@@ -176,10 +185,18 @@ def reset(update: Update, context: CallbackContext):
 
 
 def callback_shipping():
-    global shippable
-    shippable = True
     global deadline
     deadline += timedelta(days=1)
+    with open('counters.json', 'r') as counters_fp:
+        try:
+            global chat_id
+            counters = json.load(counters_fp)
+            counters[chat_id]['shippable'] = True
+            with open('counters.json', 'w') as _counters_fp:
+                json.dump(counters, _counters_fp)
+        except json.JSONDecodeError as e:
+            logging.error("Couldn't open file")
+
 
 
 def main():
