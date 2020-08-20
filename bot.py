2d583716e046f13dd89d97f3db72d66595b491d8
plutone <borelliplutone@gmail.com>
2020-08-20 11:56:53 +0200

    redis calls replaced json file

--- bot.py
+++ bot.py
@@ -27,6 +27,7 @@ victory_text = ''
 redis_server = redis.from_url(os.getenv('REDIS_URL'))
 PORT = int(os.environ.get('PORT', 5000))
 
+
 def setup_shippering_db(update: Update, context: CallbackContext):
 
     if update.effective_chat.type != 'private':
@@ -40,7 +41,7 @@ def setup_shippering_db(update: Update, context: CallbackContext):
             chat_ship[update.effective_chat.id]['user_counters'][chat_member.user.id] = 0
         chat_ship[update.effective_chat.id]['last_couple'] = []
         # set a single key containing an object like string, if the key doesn't exist already
-        redis_server.setnx(str(update.effective_chat.id), str(chat_ship[update.effective_chat.id]))
+        redis_server.setnx(str(update.effective_chat.id), json.dumps(chat_ship[update.effective_chat.id]))
 
 
 def victory(update: Update, context: CallbackContext, winner1, winner2=None):
@@ -96,53 +97,83 @@ def shipping(update: Update, context: CallbackContext):
     global victory_text
 
     setup_shippering_db(update, context)
-    with open('counters.json') as counters_fp:
-        try:
-            counters = json.load(counters_fp)
 
-        except json.JSONDecodeError as e:
-            logging.error("UNABLE TO READ FILE")
-            context.bot.send_message(chat_id=update.effective_chat.id, text='Oops, something went wrong 😅', parse_mode='HTML')
+    counters = redis_server.get(str(update.effective_chat.id))
+    counters = json.loads(counters)
 
-        if counters[str(update.effective_chat.id)]['shippable']:
-            # last key is not counted for randomization since it's the last ship
-            ship1, ship2 = tuple(
-                random.sample(range(0, len(counters[str(update.effective_chat.id)]['user_counters'].keys())), k=2))
+    if counters['shippable']:
+        # last key is not counted for randomization since it's the last ship
+        ship1, ship2 = tuple(
+            random.sample(range(0, len(counters['user_counters'].keys())), k=2))
 
-            # find the id whose index in the key list is the rng number
-            user_id_shipped1 = list(counters[str(update.effective_chat.id)]['user_counters'].items())[ship1][0]
-            user_id_shipped2 = list(counters[str(update.effective_chat.id)]['user_counters'].items())[ship2][0]
+        # find the id whose index in the key list is the rng number
+        user_id_shipped1 = list(counters['user_counters'].items())[ship1][0]
+        user_id_shipped2 = list(counters['user_counters'].items())[ship2][0]
 
-            counters[str(update.effective_chat.id)]['user_counters'][user_id_shipped1] += 1
-            counters[str(update.effective_chat.id)]['user_counters'][user_id_shipped2] += 1
-            counters[str(update.effective_chat.id)]['last_couple'].append(user_id_shipped1)
-            counters[str(update.effective_chat.id)]['last_couple'].append(user_id_shipped2)
-            if len(counters[str(update.effective_chat.id)]['last_couple']) > 10:
-                counters[str(update.effective_chat.id)]['last_couple'].pop(0)
-                counters[str(update.effective_chat.id)]['last_couple'].pop(0)
-            # if someone reached 30, winner
-            winners = []
-            if counters[str(update.effective_chat.id)]['user_counters'][user_id_shipped1] >= VICTORY:
-                winners.append(user_id_shipped1)
-            if counters[str(update.effective_chat.id)]['user_counters'][user_id_shipped2] >= VICTORY:
-                winners.append(user_id_shipped2)
-            if len(winners) > 0:
-                victory_text = victory(update, context, *winners)
+        counters['user_counters'][user_id_shipped1] += 1
+        counters['user_counters'][user_id_shipped2] += 1
+        counters['last_couple'].append(user_id_shipped1)
+        counters['last_couple'].append(user_id_shipped2)
+        if len(counters['last_couple']) > 10:
+            counters['last_couple'].pop(0)
+            counters['last_couple'].pop(0)
+        # if someone reached 30, winner
+        winners = []
+        if counters['user_counters'][user_id_shipped1] >= VICTORY:
+            winners.append(user_id_shipped1)
+        if counters['user_counters'][user_id_shipped2] >= VICTORY:
+            winners.append(user_id_shipped2)
+        if len(winners) > 0:
+            victory_text = victory(update, context, *winners)
 
 
 
-        else:
-            # if the ship for today is picked, only need to pop from the stack
-            user_id_shipped2 = counters[str(update.effective_chat.id)]['last_couple'][-1]
-            user_id_shipped1 = counters[str(update.effective_chat.id)]['last_couple'][-2]
+    else:
+        # if the ship for today is picked, only need to pop from the stack
+        user_id_shipped2 = counters['last_couple'][-1]
+        user_id_shipped1 = counters['last_couple'][-2]
 
-        # find out how much needs to be waited to ship again
-        now = datetime.today()
-        time_to_wait = deadline - now
-        total_seconds = time_to_wait.seconds + math.floor(round(time_to_wait.microseconds / 1000000, 1))
-        hours, remaining_seconds = divmod(total_seconds, 3600)
-        minutes, seconds = divmod(remaining_seconds, 60)
+    # find out how much needs to be waited to ship again
+    now = datetime.today()
+    time_to_wait = deadline - now
+    total_seconds = time_to_wait.seconds + math.floor(round(time_to_wait.microseconds / 1000000, 1))
+    hours, remaining_seconds = divmod(total_seconds, 3600)
+    minutes, seconds = divmod(remaining_seconds, 60)
 
+    first_name1 = context.bot.get_chat_member(chat_id=update.effective_chat.id,
+                                              user_id=user_id_shipped1).user.first_name
+    last_name1 = context.bot.get_chat_member(chat_id=update.effective_chat.id,
+                                             user_id=user_id_shipped1).user.last_name
+    first_name2 = context.bot.get_chat_member(chat_id=update.effective_chat.id,
+                                              user_id=user_id_shipped2).user.first_name
+    last_name2 = context.bot.get_chat_member(chat_id=update.effective_chat.id,
+                                           user_id=user_id_shipped2).user.last_name
+    text = ''
+    if victory_text:
+        text += victory_text + '\n\n'
+    text += 'La coppia del giorno:\n\n' if counters['shippable'] else 'La coppia del giorno è già stata scelta:\n\n'
+    text += f'<a href="tg://user?id={user_id_shipped1}">{first_name1} {last_name1}</a> ' if last_name1 else f'<a href="tg://user?id={user_id_shipped1}">{first_name1}</a>'
+    text += f'+ <a href="tg://user?id={user_id_shipped2}">{first_name2} {last_name2}</a> = ❤\n' if last_name2 else f'+ <a href="tg://user?id={user_id_shipped2}">{first_name2}</a> = ❤\n'
+    text += f'La nuova coppia del giorno potrà essere scelta tra {hours} ore, {minutes} minuti e {seconds} secondi'
+
+    counters['shippable'] = False
+    # write updates
+    redis_server.set(str(update.effective_chat.id), json.dumps(counters))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode='HTML')
+
+
+def last_ship(update: Update, context: CallbackContext):
+    setup_shippering_db(update, context)
+
+    text = 'Le coppie scelte negli ultimi giorni:\n\n'
+
+    counters = redis_server.get(str(update.effective_chat.id))
+    counters = json.loads(counters)
+
+    last_couple_stack = counters['last_couple']
+    for i in range(0, len(counters['last_couple']), 2):
+        user_id_shipped2 = last_couple_stack.pop()
+        user_id_shipped1 = last_couple_stack.pop()
         first_name1 = context.bot.get_chat_member(chat_id=update.effective_chat.id,
                                                   user_id=user_id_shipped1).user.first_name
         last_name1 = context.bot.get_chat_member(chat_id=update.effective_chat.id,
@@ -150,75 +181,46 @@ def shipping(update: Update, context: CallbackContext):
         first_name2 = context.bot.get_chat_member(chat_id=update.effective_chat.id,
                                                   user_id=user_id_shipped2).user.first_name
         last_name2 = context.bot.get_chat_member(chat_id=update.effective_chat.id,
-                                               user_id=user_id_shipped2).user.last_name
-        text = ''
-        if victory_text:
-            text += victory_text + '\n\n'
-        text += 'La coppia del giorno:\n\n' if counters[str(update.effective_chat.id)][
-            'shippable'] else 'La coppia del giorno è già stata scelta:\n\n'
-        text += f'<a href="tg://user?id={user_id_shipped1}">{first_name1} {last_name1}</a> ' if last_name1 else f'<a href="tg://user?id={user_id_shipped1}">{first_name1}</a>'
-        text += f'+ <a href="tg://user?id={user_id_shipped2}">{first_name2} {last_name2}</a> = ❤\n' if last_name2 else f'+ <a href="tg://user?id={user_id_shipped2}">{first_name2}</a> = ❤\n'
-        text += f'La nuova coppia del giorno potrà essere scelta tra {hours} ore, {minutes} minuti e {seconds} secondi'
+                                                 user_id=user_id_shipped2).user.last_name
+        text += '❤ = '
+        text += f'{first_name1} {last_name1} + ' if last_name1 else f'{first_name1} + '
+        text += f'{first_name2} {last_name2}\n' if last_name2 else f'{first_name2}\n'
 
-        counters[str(update.effective_chat.id)]['shippable'] = False
-        # write updates
-        with open('counters.json', 'w') as _counters_fp:
-            json.dump(counters, _counters_fp)
-        context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode='HTML')
-
-
-def last_ship(update: Update, context: CallbackContext):
-    setup_shippering_db(update, context)
-    with open('counters.json') as counters_fp:
-        text = 'Le coppie scelte negli ultimi giorni:\n\n'
-        counters = json.load(counters_fp)
-        last_couple_stack = counters[str(update.effective_chat.id)]['last_couple']
-        for i in range(0, len(counters[str(update.effective_chat.id)]['last_couple']), 2):
-            user_id_shipped2 = last_couple_stack.pop()
-            user_id_shipped1 = last_couple_stack.pop()
-            first_name1 = context.bot.get_chat_member(chat_id=update.effective_chat.id,
-                                                      user_id=user_id_shipped1).user.first_name
-            last_name1 = context.bot.get_chat_member(chat_id=update.effective_chat.id,
-                                                     user_id=user_id_shipped1).user.last_name
-            first_name2 = context.bot.get_chat_member(chat_id=update.effective_chat.id,
-                                                      user_id=user_id_shipped2).user.first_name
-            last_name2 = context.bot.get_chat_member(chat_id=update.effective_chat.id,
-                                                     user_id=user_id_shipped2).user.last_name
-            text += '❤ = '
-            text += f'{first_name1} {last_name1} + ' if last_name1 else f'{first_name1} + '
-            text += f'{first_name2} {last_name2}\n' if last_name2 else f'{first_name2}\n'
-
-        context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode='HTML')
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode='HTML')
 
 
 def top_ship(update: Update, context: CallbackContext):
     setup_shippering_db(update, context)
-    with open('counters.json') as counters_fp:
-        text = 'Top lovers (coloro scelti il maggior numero di volte):\n\n'
-        counters = json.load(counters_fp)
-        id_items = list(counters[str(update.effective_chat.id)]['user_counters'].items())
-        ranking = sorted(id_items, key=lambda id_counter : id_counter[1], reverse=True)
-        i = 1
-        for rank in ranking:
-            first_name = context.bot.get_chat_member(chat_id=update.effective_chat.id,
-                                                      user_id=rank[0]).user.first_name
-            last_name = context.bot.get_chat_member(chat_id=update.effective_chat.id,
-                                                     user_id=rank[0]).user.last_name
-            text += f'{i}. '
-            text += f'{first_name} {last_name} — <b>{rank[1]}</b>\n' if last_name else f'{first_name} — <b>{rank[1]}</b>\n'
-            i += 1
-        context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode='HTML')
+
+    text = 'Top lovers (coloro scelti il maggior numero di volte):\n\n'
+
+    counters = redis_server.get(str(update.effective_chat.id))
+    counters = json.loads(counters)
+
+    id_items = list(counters['user_counters'].items())
+    ranking = sorted(id_items, key=lambda id_counter : id_counter[1], reverse=True)
+    i = 1
+    for rank in ranking:
+        first_name = context.bot.get_chat_member(chat_id=update.effective_chat.id,
+                                                  user_id=rank[0]).user.first_name
+        last_name = context.bot.get_chat_member(chat_id=update.effective_chat.id,
+                                                 user_id=rank[0]).user.last_name
+        text += f'{i}. '
+        text += f'{first_name} {last_name} — <b>{rank[1]}</b>\n' if last_name else f'{first_name} — <b>{rank[1]}</b>\n'
+        i += 1
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text, parse_mode='HTML')
 
 
 def restart_counter(update: Update):
-    with open('counters.json') as counters_fp:
-        counters = json.load(counters_fp)
-        for user_id in counters[str(update.effective_chat.id)]['user_counters']:
-            counters[str(update.effective_chat.id)]['user_counters'][user_id] = 0
-        counters[str(update.effective_chat.id)]['last_couple'] = []
-        counters[str(update.effective_chat.id)]['shippable'] = True
-        with open('counters.json', 'w') as _counters_fp:
-            json.dump(counters, _counters_fp)
+    counters = redis_server.get(str(update.effective_chat.id))
+    counters = json.loads(counters)
+
+    for user_id in counters['user_counters']:
+        counters['user_counters'][user_id] = 0
+    counters['last_couple'] = []
+    counters['shippable'] = True
+
+    redis_server.set(str(update.effective_chat.id), json.dumps(counters))
 
 
 def reset(update: Update, context: CallbackContext):
@@ -231,14 +233,12 @@ def reset(update: Update, context: CallbackContext):
 def callback_shipping(chat_id):
     global deadline
     deadline += timedelta(days=1)
-    with open('counters.json', 'r') as counters_fp:
-        try:
-            counters = json.load(counters_fp)
-            counters[str(chat_id)]['shippable'] = True
-            with open('counters.json', 'w') as _counters_fp:
-                json.dump(counters, _counters_fp)
-        except json.JSONDecodeError as e:
-            logging.error("Couldn't open file")
+
+    counters = redis_server.get(str(chat_id))
+    counters = json.loads(counters)
+
+    counters['shippable'] = True
+    redis_server.set(str(chat_id), json.dumps(counters))
 
 
 def run_continuously(interval=5):
@@ -254,6 +254,7 @@ def run_continuously(interval=5):
     continuous_thread = ScheduleThread()
     continuous_thread.start()
     return cease_continuous_run
+
 
 def main():
 
