3a401bc27383e7dbb8adae813345850737f037b3
plutone <borelliplutone@gmail.com>
2020-08-21 12:11:38 +0200

    using utc 0

--- bot.py
+++ bot.py
@@ -139,7 +139,7 @@ def shipping(update: Update, context: CallbackContext):
 
     logging.info('DEADLINE E SCHEDULE DOVREBBERO COINCIDERE')
     logging.info(str(schedule.next_run()))
-    now = datetime.utcnow() + timedelta(hours=2)
+    now = datetime.utcnow()
 
     logging.info('ADESSO')
     logging.info(str(now.date()))
