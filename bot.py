37363310e09896c78ad6319e4a7b72a2d9b18b93
plutone <borelliplutone@gmail.com>
2020-08-23 17:31:31 +0200

    fixed missing parameter

--- bot.py
+++ bot.py
@@ -272,7 +272,7 @@ def run_continuously(interval=1):
     return cease_continuous_run
 
 
-def ping_message_handler(update: Update, context: CallbackContext):
+def ping_message(update: Update, context: CallbackContext):
     logging.info('KEEP ALIVE')
     logging.info(schedule.next_run())
 
@@ -291,7 +291,7 @@ def main():
     last_ship_handler = CommandHandler('last', last_ship, Filters.group)
     top_ship_handler = CommandHandler('top', top_ship, Filters.group)
     reset_handler = CommandHandler('reset', reset, Filters.group)
-    ping_message_handler = MessageHandler(Filters.text, )
+    ping_message_handler = MessageHandler(Filters.text, ping_message)
 
     dispatcher.add_handler(start_handler)
     dispatcher.add_handler(help_handler)
